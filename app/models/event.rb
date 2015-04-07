dc47d2f20c08280025e7470674447ad3d8d53b7f
mclacs14 <bradleyg36@gmail.com>
2015-04-07 11:16:36 -0400

    Added Image Upload to Events

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,6 @@
 class Event < ActiveRecord::Base
 	has_and_belongs_to_many:artists
 	has_and_belongs_to_many:arts
+	has_attached_file :image, styles: {large: "600x600#" , medium:"300x300>", thumb:"100x100#" }
+  	validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150405212748) do
+ActiveRecord::Schema.define(version: 20150407140009) do
 
   create_table "artists", force: :cascade do |t|
     t.string   "name"
@@ -55,8 +55,12 @@ ActiveRecord::Schema.define(version: 20150405212748) do
 
   create_table "events", force: :cascade do |t|
     t.string   "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
 end
